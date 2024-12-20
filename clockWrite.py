ac7e5b44cb35e753f1eb0fa59e9a0c8acc3026e7
aarushth <65953492+aarushth@users.noreply.github.com>
2024-12-20 11:16:19 -0800

    Add files via upload

---
+++ clockWrite.py
@@ -0,0 +1,10 @@
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser  # ver. < 3.0
+
+# instantiate
+config = ConfigParser()
+
+# parse existing file
+config.read('Clock.ini')
