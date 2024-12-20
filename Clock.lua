09db48eef334106ded86b0a2766e1a75210bf679
aarushth <aarush.thadani@outlook.com>
2024-12-20 14:59:42 -0800

    done??

--- Clock.lua
+++ Clock.lua
@@ -1,55 +1,117 @@
 function Initialize()
-	Oldtime4 = SKIN:GetMeasure("MeasureTime"):GetValue()
+	Oldtime = {SKIN:GetMeasure("MeasureTime"):GetValue(),SKIN:GetMeasure("MeasureTime"):GetValue(),SKIN:GetMeasure("MeasureTime"):GetValue(),SKIN:GetMeasure("MeasureTime"):GetValue()}
+	
+	
 	Arr =  {{{{90,45,90,90,90,90,90,90,90,90},{180,45,180,180,180,180,180,180,180,180}},{{90,90,90,90,180,90,90,90,90,90},{270,180,270,270,270,270,270,270,270,270}},{{90,90,90,90,90,90,90,90,90,90},      {270,270,270,270,180,270,270,270,270,270}},{{180, 180, 180, 180, 180, 180, 180, 180, 180, 180},{270,270,270,270,270,270,270,270,270,270}}},
 			{{{0,45,0,0,0,0,0,0,0,0},         {180,45,90,90,180,180,180,90,180,180}},   {{90,0,90,90,0,90,90,90,90,90},   {180,90,270,270,180,180,180,270,180,180}}, {{180,180,180,180,0,90,90,180,180,180},{270,270,270,270,180,270,270,270,270,270}},{{0,0,0,0,0,0,0,0,0,0},                             {180,180,180,180,180,270,270,180,180,180}}},
 			{{{0,45,90,90,0,0,0,45,0,0},      {180,45,180,180,180,180,180,45,135,180}}, {{0,45,90,90,0,0,0,45,0,0},       {180,45,270,270,90,90,90,45,90,90}},       {{0,0,0,0,0,90,90,0,0,0},              {180,180,270,270,270,270,270,180,270,270}},{{0,0,0,0,0,180,180,0,0,0},                         {180,180,180,180,180,270,270,180,225,180}}},
 			{{{0,45,0,0,0,0,0,45,45,0},       {180,45,180,90,90,90,180,45,180,90}},     {{0,45,90,90,90,90,90,45,90,90},  {180,45,180,270,270,270,180,45,180,270}},  {{0,0,90,180,180,180,180,0,180,180},   {180,180,270,270,270,270,270,180,270,270}},{{0,0,0,0,0,0,0,0,180,0},                           {180,180,270,180,180,180,180,180,315,180}}},
 			{{{0,45,0,90,45,90,0,45,0,90},    {180,45,180,180,45,180,180,45,180,180}},  {{0,45,0,90,45,90,0,45,0,90},     {90,45,90,270,45,270,90,45,90,270}},       {{0,0,90,0,0,0,0,0,0,0},               {270,180,270,270,180,270,270,180,270,270}},{{0,0,180,0,0,0,0,0,0,0},                           {180,180,270,180,180,180,180,180,180,180}}},
 			{{{0,45,0,0,45,0,0,45,0,0},       {90,45,90,90,45,90,90,45,90,90}},         {{90,45,90,90,45,90,90,45,90,90}, {270,45,270,270,45,270,270,45,270,270}},   {{90,0,90,90,0,90,90,0,90,90},         {270,90,270,270,90,270,270,90,270,270}},   {{0,0,0,0,0,0,0,0,0,0},                             {270,270,270,270,270,270,270,270,270,270}}}}
+	
+	Midarr =   {{{90,180},{180,270}},
+				{{0,180}, {0,180}},
+				{{0,90},  {0,270}},
+				{{90,180},{180,270}},
+				{{0,180}, {0,180}},
+				{{0,90},  {180,270}}}
+	
+	T = {0,0,0,0,0}
+	Tmid = 0
+	
+	Time = {nil, nil, nil, nil}
+	Rotating = {false, false, false, false}
+	
+	scale = SKIN:GetVariable('PosScale')
+	offset = 0
+
 	Oldarr = {}
-	T = 0
 	for i=1,4 do
 		Oldarr[i] = {}
+		if(i>=3) then
+			offset = 2*scale
+		end
 		for j=1,6 do
 			Oldarr[i][j] = {}
 			for k=1,4 do
 				Oldarr[i][j][k] = {}
 				for l=1,2 do
 					Oldarr[i][j][k][l] = nil
+					SKIN:GetMeter('Meter'..i..j..k..l):SetY((j-1)*scale)
+					SKIN:GetMeter('Meter'..i..j..k..l):SetX(((i-1)*200)+((k-1)*50)+offset)
 				end
 			end
 		end
 	end
-	-- Oldarr[4][0][3][0] = SKIN:GetMeasure('Measure4030'):GetNumberOption('Formula')
-	-- If the "Old" value starts as nil, the change action will always fire on the first update.
+	
+	Oldmidarr = {}
+	for i=1,6 do
+		Oldmidarr[i] = {}
+		for j=1,2 do
+			Oldmidarr[i][j] = {}
+			for k=1,2 do
+				Oldmidarr[i][j][k] = nil
+				SKIN:GetMeter('MeterMid'..i..j..k):SetY((i-1)*scale)
+				SKIN:GetMeter('MeterMid'..i..j..k):SetX(+((j-1)*50)+offset)
+			end
+		end
+	end
 end
 
 function Update()
-
-	Time4 = math.floor((SKIN:GetMeasure("MeasureTime"):GetValue())/60)%10
-	if(Oldtime4 ~= Time4) then
-		Oldtime4 = Time4
-		for i=1,6 do
-			for j=1,4 do
-				for k=1,2 do
-					Oldarr[4][i][j][k] = SKIN:GetMeasure('Measure4'..i..j..k):GetNumberOption('Formula')
+	Time[1] = math.floor((SKIN:GetMeasure("MeasureTime"):GetValue())/36000)%10
+	Time[2] = math.floor((SKIN:GetMeasure("MeasureTime"):GetValue())/3600)%10
+	Time[3] = math.floor((SKIN:GetMeasure("MeasureTime"):GetValue())/600)%10
+	Time[4] = math.floor((SKIN:GetMeasure("MeasureTime"):GetValue())/60)%10
+	for li=1,4 do
+		if(Oldtime[li] ~= Time[li]) then
+			Oldtime[li] = Time[li]
+			for i=1,6 do
+				for j=1,4 do
+					for k=1,2 do
+						Oldarr[li][i][j][k] = SKIN:GetMeasure('Measure'..li..i..j..k):GetNumberOption('Formula')
+					end
 				end
 			end
+			Rotating[li] = true
+			if(li<=2) then
+				for i=1,6 do
+					for j=1,2 do
+						for k=1,2 do
+							Oldmidarr[i][j][k] = SKIN:GetMeasure('MeasureMid'..i..j..(3-k)):GetNumberOption('Formula')
+						end
+					end
+				end
+				Rotatingmid = true
+			end
 		end
-		Rotating4 = true
+		if(Rotating[li]) then
+			T[li] = T[li] + 0.005
+			for i=1,6 do
+				for j=1,4 do
+					for k=1,2 do
+						SKIN:Bang('!SetOption', 'Measure'..li..i..j..k, 'Formula', Lerp(Oldarr[li][i][j][k], Arr[i][j][k][Oldtime[li]+1]))
+					end
+				end
+			end
+			if(T[li] >= 1) then
+				Rotating[li] = false
+				T[li] = 0
+			end
+		end
 	end
-    if(Rotating4) then
-		T = T + 0.005
+	if(Rotatingmid) then
+		Tmid = Tmid + 0.005
 		for i=1,6 do
-			for j=1,4 do
+			for j=1,2 do
 				for k=1,2 do
-					SKIN:Bang('!SetOption', 'Measure4'..i..j..k, 'Formula', Lerp(Oldarr[4][i][j][k], Arr[i][j][k][Oldtime4+1]))
+					SKIN:Bang('!SetOption', 'MeasureMid'..i..j..k, 'Formula', Lerp(Oldmidarr[i][j][k], Midarr[i][j][k]))
 				end
 			end
 		end
-		if(T >= 1) then
-			Rotating4 = false
-			T = 0
+		if(Tmid >= 1) then
+			Rotating[li] = false
+			Tmid = 0
 		end
 	end
 end
---
+++ test.py
@@ -0,0 +1,4 @@
+import time
+import math
+
+print((math.floor(time.time()/3600)%24)%10)
